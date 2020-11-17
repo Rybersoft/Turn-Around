5163c4346f6f5fa145b343ca3666c35e0bde92c1
Rishabh Ryber <rishabhryber@gmail.com>
2020-11-17 11:51:01 +0530

    basic working version done

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameController : MonoBehaviour
 {
-    [SerializeField]GameObject player;
+    [SerializeField]Player player;
+    // used to refer the next direction to turn into
+    // 0-left 1 right
+    int nextDirection = 1 ;
+    double speed = 1.0f;
     Rigidbody playerRBody;
     // Start is called before the first frame update
     void Start()
@@ -14,8 +17,19 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+        // detect if the player falls off the boundry
+        if(player.isFalling())
+            gameOver();
+        if(Input.anyKeyDown){
+            if(nextDirection == 1){
+                player.turnRight();
+            }else{
+                player.turnLeft();
+            }
+        }
     }
-
-    FixedU
+    void gameOver(){
+        // tobe implemented later
+        Debug.Log("Game Over");
+    }
 }
