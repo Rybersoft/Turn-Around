e69f01e09f7aebaa0d7f439c53c84767f0c3d77e
namita2310 <namitashetty01@gmail.com>
2020-11-17 23:38:21 +0530

    movements changed

--- Assets/scripts/movementController.cs
+++ Assets/scripts/movementController.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class movementController : MonoBehaviour
 {
-    public Animator anim;
-    // Start is called before the first frame update
-    
-    void Update()
-    {
-        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
-        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
-    }
+	float speed = 4;
+	float rotSpeed = 100;
+	float rot = 0f;
+	float gravity = 8;
+
+
+	Vector3 moveDir = Vector3.zero;
+
+
+	CharacterController controller;
+	Animator anim;
+
+
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+
+		controller = GetComponent<CharacterController>();
+		anim = GetComponent<Animator>();
+	}
+
+
+	// Update is called once per frame
 }
---
+++ Assets/scripts/player1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime);
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+    public void moveForward(double speed)
+    {
+        rb.velocity = transform.forward;
+    }
+    public void turnRight()
+    {
+        rb.transform.Rotate(0.0f, 90.0f, 0.0f, Space.World);
+    }
+    public void turnLeft()
+    {
+        rb.transform.Rotate(0.0f, -90.0f, 0.0f, Space.World);
+
+    }
+    public bool isFalling()
+    {
+        return rb.position.y < 0;
+    }
+}
